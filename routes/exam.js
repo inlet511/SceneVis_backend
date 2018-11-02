468bbeecc810bd082518773f7f8a5301b8741b2d
inlet511 <80669349@qq.com>
2018-11-02 16:53:45 +0800

    加入成绩查询

--- routes/exam.js
+++ routes/exam.js
@@ -1,21 +1,54 @@
 const express = require('express');
 const examModel = require('../models/exam');
-const userModel = require('../models/user');
-const moment = require('moment');
 
 const route = express.Router();
 
-route.get('/',(req,res,next)=>{
-    examModel.find({}).populate('sceneID','SceneName').populate('userID','name').then((exams)=>{
+// 网页显示所有考核成绩
+route.get('/', (req, res, next) => {
+    //populate 联表查询， 只取场景名称和用户名称两个字段
+    examModel
+    .find({})
+    .populate('sceneID', 'SceneName')
+    .populate('userID', 'name')
+    .then((exams) => {
         res.render("exams", { "exams": exams });
     }).catch(next);
 });
 
-route.post('/',(req,res,next)=>{
-    examModel.create(req.body).then((exam)=>{
+//添加考试结果
+route.post('/', (req, res, next) => {
+    examModel.create(req.body).then((exam) => {
         console.log(req.body);
         res.send(exam);
     }).catch(next);
 });
+
+// 返回json格式的所有考核成绩的
+route.get('/json', (req, res, next) => {
+    //populate 联表查询， 只取场景名称和用户名称两个字段
+    examModel
+    .find({})
+    .populate('sceneID', 'SceneName')
+    .populate('userID', 'name')
+    .then((exams) => {
+        res.send(exams);
+    }).catch(next);
+});
+
+route.get('/findByUser',(req,res,next)=>{
+    res.render("findexams.ejs");
+});
+
+route.post('/findByUser',(req,res,next)=>{
+    examModel.find({})
+    .populate('sceneID', 'SceneName')
+    .populate('userID','name')
+    .findOne({'userID.name':req.params.userName})
+    .then((result)=>{
+        res.send(result);
+    }).catch(next);
+});
+
+
 
 module.exports = route;

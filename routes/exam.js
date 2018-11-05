bc8c444187abd8efe73bf425cc26c50dc85d9121
inlet511 <80669349@qq.com>
2018-11-05 13:56:13 +0800

    按名字查询成绩完成

--- routes/exam.js
+++ routes/exam.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const examModel = require('../models/exam');
+const userModel = require('../models/user');
 
 const route = express.Router();
 
@@ -7,12 +8,12 @@ const route = express.Router();
 route.get('/', (req, res, next) => {
     //populate 联表查询， 只取场景名称和用户名称两个字段
     examModel
-    .find({})
-    .populate('sceneID', 'SceneName')
-    .populate('userID', 'name')
-    .then((exams) => {
-        res.render("exams", { "exams": exams });
-    }).catch(next);
+        .find({})
+        .populate('sceneID', 'SceneName')
+        .populate('userID', 'name')
+        .then((exams) => {
+            res.render("exams", { "exams": exams });
+        }).catch(next);
 });
 
 //添加考试结果
@@ -27,28 +28,36 @@ route.post('/', (req, res, next) => {
 route.get('/json', (req, res, next) => {
     //populate 联表查询， 只取场景名称和用户名称两个字段
     examModel
-    .find({})
-    .populate('sceneID', 'SceneName')
-    .populate('userID', 'name')
-    .then((exams) => {
-        res.send(exams);
-    }).catch(next);
+        .find({})
+        .populate('sceneID', 'SceneName')
+        .populate('userID', 'name')
+        .then((exams) => {
+            res.send(exams);
+        }).catch(next);
 });
 
-route.get('/findByUser',(req,res,next)=>{
+route.get('/findByUser', (req, res, next) => {
     res.render("findexams.ejs");
 });
 
-route.post('/findByUser',(req,res,next)=>{
-    examModel.find({})
-    .populate('sceneID', 'SceneName')
-    .populate('userID','name')
-    .findOne({'userID.name':req.params.userName})
-    .then((result)=>{
-        res.send(result);
-    }).catch(next);
+route.post('/findByUser', (req, res, next) => {
+    var foundUserID='';
+    userModel.findOne({name:req.body.userName})
+    .then(
+        userResult=>{
+            foundUserID = userResult._id;
+            examModel.find({userID:foundUserID}).populate('sceneID','SceneName').then(
+                foundExams=>{
+                    var output={};
+                    output.userName = req.body.userName;
+                    output.exams = foundExams;
+                    console.log(output);
+                    res.send(output);
+                }
+            )
+        }
+    ).catch(next);
 });
-
 
 
 module.exports = route;
